dddbab1d9946b388bcf24d67d12987b67a3b945f
DrChinaWhite <drhawkins@mta.ca>
2021-09-05 15:05:21 -0400

    Add files via upload

---
+++ Assignment 4/src/js/app.js
@@ -0,0 +1,85 @@
+// An instance of our SimpleMap, created below when the window loads.
+let map;
+
+// Update the map to show markers for the set of observations
+function updateMap(observations, map) {
+  // Clear the current markers on the map (if any)
+  map.clear();
+
+  observations.forEach(function (observation) {
+    map.addObservation(observation);
+  });
+}
+
+// Update the table to show markers for the set of observations
+function updateTable(observations) {
+  // Remove any current data from the table
+  clearAllTableRows();
+
+  // Populate the table with all observation data we want to show
+  observations.forEach((observation) => {
+    let newRow = buildRowForObservation(observation);
+    addRowToTable(newRow);
+  });
+}
+
+// Show all species on the map and table
+function showAll() {
+  // Get all the observations from our data.js and format them so we can work with the data
+  const observations = getAllObservations();
+
+  // Update the map and table
+  updateMap(observations, map);
+  updateTable(observations);
+  updateTableTitle(`All Species (${observations.length})`);
+}
+
+// Show native species on the map and table
+function showOnlyNative() {
+  // Get all the observations from our data.js and format them so we can work with the data
+  const observations = getAllObservations();
+  // Filter out any that aren't native species
+  const native = filterOnlyNative(observations);
+
+  // Update the map and table
+  updateMap(native, map);
+  updateTable(native);
+  updateTableTitle(`Only Native Species (${native.length})`);
+}
+
+// Show introduced species on the map and table
+function showOnlyIntroduced() {
+  // Get all the observations from our data.js and format them so we can work with the data
+  const observations = getAllObservations();
+  // Filter out any that aren't introduced species
+  const introduced = filterOnlyIntroduced(observations);
+  // Update the map and table
+  updateMap(introduced, map);
+  updateTable(introduced);
+  updateTableTitle(`Only Introduced Species (${introduced.length})`);
+}
+
+function start() {
+  // Create our map object for Seneca's Newnham campus
+  map = new SimpleMap("map-container", 43.7955, -79.3496);
+  
+  let button = document.getElementById("show-all");
+  button.onClick = (event) => {
+    showAll(); 
+  };
+  let button2 = document.getElementById("show-native");
+  button2.onClick = (event) => {
+    showOnlyNative();
+  };
+  let button3 = document.getElementById("show-introduced")
+  button3.onClick = (event) => {
+    showOnlyIntroduced();
+  };
+  
+  // Show all species observations by default when we start.
+  showAll();
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+ start();
+});
